e027e01ab6ec3e8bbaa9868702359ae6d4538c59
xyqyear <xyqyear@gmail.com>
2023-09-03 22:05:48 +0800

    add more thoughts

--- mc_router_dns_manager/service_monitor.py
+++ mc_router_dns_manager/service_monitor.py
@@ -4,9 +4,12 @@ This class should be responsible for the following:
 - listens to ws events from those services as well
 - whenever there is an event (be it from ws or from polling)
     - pull info from dns and mc-router
-    - checks if there is any change
+    - checks if server_list from dns matches the ones from mc-router
+    - also checks if there is any change
     - if there is a change, call the callback function
 
 Specifically, when initializing, it should check for changes once 
     and call the callback function if there is any change (indefinitely retrying if there is an error)
+
+I think there should also be a queue for events that are not yet processed
 """
