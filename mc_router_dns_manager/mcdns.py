e270c46da6fd5e7f3ef1395a5e2f4a2e7dbdd86f
xyqyear <xyqyear@gmail.com>
2023-09-05 10:12:28 +0800

    change to a "set and push, pull and get" paradigm

--- mc_router_dns_manager/mcdns.py
+++ mc_router_dns_manager/mcdns.py
@@ -1,9 +1,10 @@
-from .dns import DNSClient, ReturnRecordT, RecordListT, AddRecordListT, AddRecordT
-from typing import TypedDict, Literal
 import asyncio
+from typing import Literal, NamedTuple
+
+from .dns import AddRecordListT, AddRecordT, DNSClient, RecordListT, ReturnRecordT
 
 
-class AddressInfoT(TypedDict):
+class AddressInfoT(NamedTuple):
     type: Literal["A", "AAAA", "CNAME"]
     host: str
     port: int
@@ -12,7 +13,7 @@ class AddressInfoT(TypedDict):
 AddressesT = dict[str, AddressInfoT]
 
 
-class SrvParsedResultT(TypedDict):
+class SrvParsedResultT(NamedTuple):
     server_name: str
     address_name: str
     port: int
@@ -44,19 +45,59 @@ the `address_name` is another whole mess
 
 
 class MCDNSClient:
-    def __init__(self, dns_client: DNSClient, managed_sub_domain: str):
+    def __init__(self, dns_client: DNSClient, managed_sub_domain: str, ttl: int = 600):
         self._dns_client = dns_client
         self._managed_sub_domain = managed_sub_domain
+        self._ttl = ttl
         self._addresses = AddressesT()
         self._server_list = list[str]()
 
         self._dns_update_lock = asyncio.Lock()
 
+    async def _get_relevent_records(self) -> RecordListT:
+        record_list = await self._dns_client.list_records()
+        relevent_records = RecordListT()
+        for record in record_list:
+            if (
+                record.record_type
+                in (
+                    "A",
+                    "AAAA",
+                    "CNAME",
+                )
+                and record.sub_domain.startswith("*")
+                and record.sub_domain.endswith(f".{self._managed_sub_domain}")
+            ):
+                relevent_records.append(record)
+            elif (
+                record.record_type == "SRV"
+                and record.sub_domain.startswith(f"_minecraft._tcp.")
+                and record.sub_domain.endswith(f".{self._managed_sub_domain}")
+            ):
+                relevent_records.append(record)
+        return relevent_records
+
+    async def _remove_relevent_records(self):
+        records = await self._get_relevent_records()
+        record_ids = [record.record_id for record in records]
+        if record_ids:
+            await self._dns_client.remove_records(record_ids)
+
+    def set_dns_client(self, dns_client: DNSClient):
+        """
+        only sets the internal dns client, does not push or pull
+        for test or migration purposes
+        """
+        self._dns_client = dns_client
+
+    def set_ttl(self, ttl: int):
+        self._ttl = ttl
+
     def _parse_srv_record(self, record: ReturnRecordT) -> SrvParsedResultT:
         """
         parse the srv record value to a tuple of (target, port)
         """
-        _, _, port, server_host = record["value"].split(" ")
+        _, _, port, server_host = record.value.split(" ")
         full_sub_domain = server_host.split(
             f".{self._managed_sub_domain}.{self._dns_client.get_domain()}"
         )[0]
@@ -69,121 +110,100 @@ class MCDNSClient:
             server_name=server_name, address_name=address_name, port=int(port)
         )
 
-    def _generate_records(self) -> AddRecordListT:
-        records = AddRecordListT()
-        for address_name, address_info in self._addresses.items():
-            if address_name == "*":
-                sub_domain_base = f"{self._managed_sub_domain}"
-            else:
-                sub_domain_base = f"{address_name}.{self._managed_sub_domain}"
-            records.append(
-                AddRecordT(
-                    sub_domain=f"*.{sub_domain_base}",
-                    value=address_info["host"],
-                    record_type=address_info["type"],
-                    ttl=600,
-                )
-            )
-            for server_name in self._server_list:
-                records.append(
-                    AddRecordT(
-                        sub_domain=f"_minecraft._tcp.{server_name}.{sub_domain_base}",
-                        value=f"0 5 {address_info['port']} {server_name}.{sub_domain_base}.{self._dns_client.get_domain()}.",
-                        record_type="SRV",
-                        ttl=600,
-                    )
-                )
-
-        return records
-
-    async def _get_relevent_records(self) -> RecordListT:
-        records = await self._dns_client.list_records()
-        relevent_records = RecordListT()
-        for record in records:
-            if record["record_type"] in ("A", "AAAA", "CNAME") and record[
-                "sub_domain"
-            ].endswith(f".{self._managed_sub_domain}"):
-                relevent_records.append(record)
-            elif (
-                record["record_type"] == "SRV"
-                and record["sub_domain"].startswith(f"_minecraft._tcp.")
-                and record["sub_domain"].endswith(f".{self._managed_sub_domain}")
-            ):
-                relevent_records.append(record)
-        return relevent_records
-
-    async def _remove_relevent_records(self):
-        records = await self._get_relevent_records()
-        record_ids = [record["record_id"] for record in records]
-        if record_ids:
-            await self._dns_client.remove_records(record_ids)
-
-    def set_dns_client(self, dns_client: DNSClient):
-        """
-        only sets the internal dns client, does not push or pull
-        for test or migration purposes
-        """
-        self._dns_client = dns_client
-
-    async def pull(self):
-        """
-        pull the address list from the dns record
-        """
-        record_list = await self._get_relevent_records()
+    def _from_record_list(self, record_list: RecordListT):
         self._server_list.clear()
         self._addresses.clear()
 
         port_map = dict[str, int]()
         for record in record_list:
-            if record["record_type"] == "SRV":
+            if record.record_type == "SRV":
                 parsed_result = self._parse_srv_record(record)
-                if parsed_result["server_name"] not in self._server_list:
-                    self._server_list.append(parsed_result["server_name"])
-                port_map[parsed_result["address_name"]] = parsed_result["port"]
-            else:
-                address_name = record["sub_domain"].split(".")[-2]
-                # record_type must be A or AAAA or CNAME
-                # chill pylance
+                if parsed_result.server_name not in self._server_list:
+                    self._server_list.append(parsed_result.server_name)
+                port_map[parsed_result.address_name] = parsed_result.port
+            # if it's not a srv record, then it must be a A/AAAA/CNAME record
+            # we do this to just make pylance happy
+            elif record.record_type in ("A", "AAAA", "CNAME"):
+                address_name = record.sub_domain.split(".")[-2]
                 self._addresses[address_name] = AddressInfoT(
-                    type=record["record_type"],  # type: ignore
-                    host=record["value"],
+                    type=record.record_type,
+                    host=record.value,
                     port=0,
                 )
 
+        for address_name in self._addresses.keys():
+            self._addresses[address_name] = self._addresses[address_name]._replace(
+                port=port_map[address_name]
+            )
+
+    def _to_record_list(self) -> AddRecordListT:
+        record_list = AddRecordListT()
         for address_name, address_info in self._addresses.items():
-            address_info["port"] = port_map[address_name]
+            if address_name == "*":
+                sub_domain_base = f"{self._managed_sub_domain}"
+            else:
+                sub_domain_base = f"{address_name}.{self._managed_sub_domain}"
+            record_list.append(
+                AddRecordT(
+                    sub_domain=f"*.{sub_domain_base}",
+                    value=address_info.host,
+                    record_type=address_info.type,
+                    ttl=self._ttl,
+                )
+            )
+            for server_name in self._server_list:
+                record_list.append(
+                    AddRecordT(
+                        sub_domain=f"_minecraft._tcp.{server_name}.{sub_domain_base}",
+                        value=f"0 5 {address_info.port} {server_name}.{sub_domain_base}.{self._dns_client.get_domain()}.",
+                        record_type="SRV",
+                        ttl=self._ttl,
+                    )
+                )
+
+        return record_list
+
+    async def pull(self):
+        """
+        pull the address list from the dns record
+        :raises Exception: if failed to get records from dns client
+        """
+        record_list = await self._get_relevent_records()
+        self._from_record_list(record_list)
 
     async def push(self):
         """
         sync the dns record to the address list
+        :raises Exception: if failed to update dns record
         """
         # we want to make sure only one push is running at the same time
         async with self._dns_update_lock:
             await self._remove_relevent_records()
-            await self._dns_client.add_records(self._generate_records())
+            if self._server_list and self._addresses:
+                await self._dns_client.add_records(self._to_record_list())
 
-    async def set_addresses(self, addresses: AddressesT):
+    def set_addresses(self, addresses: AddressesT):
         """
         the caller should call push() after calling this function
             in order to sync the dns record
         """
         self._addresses = addresses
 
-    async def get_addresses(self) -> AddressesT:
+    def get_addresses(self) -> AddressesT:
         """
         the caller should call pull() before calling this function
             in order to sync the dns record
         """
         return self._addresses
 
-    async def set_server_list(self, server_list: list[str]):
+    def set_server_list(self, server_list: list[str]):
         """
         the caller should call push() after calling this function
             in order to sync the dns record
         """
         self._server_list = server_list
 
-    async def get_server_list(self) -> list[str]:
+    def get_server_list(self) -> list[str]:
         """
         the caller should call pull() before calling this function
             in order to sync the dns record
